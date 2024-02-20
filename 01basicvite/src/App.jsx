33b3de3a8787f6f78d1a3b4f482ad8ef00db8559
clovis739 <clovistumasang82@gmail.com>
2024-02-20 03:46:00 +0100

    start of react series

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>welcome clovis t our vite app</h1>
+    </>
+  )
+}
+
+export default App
